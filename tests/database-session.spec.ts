335f79458450b782ee7d1ea472ea28f45e191230
Sudipto Das <38055866+Sudipta0102@users.noreply.github.com>
2026-06-26 22:43:59 +0530

    test: add comment to the test file

--- tests/database-session.spec.ts
+++ tests/database-session.spec.ts
@@ -1,5 +1,29 @@
 import { test, expect } from "../src/fixtures/database.fixture.js";
 
+/**
+ * serial mode is used just to test trnsaction-per-test which is defined 
+ * in fixture. So creating an user at 2nd test and verifying that same 
+ * username doe not exist in third test, which confirms ROLLBACK success.
+
+ * As this framework uses no temporary SQL dumps and only uses 
+ * Database Transactions. 
+ * This pattern gives you:
+ *
+ *  BEGIN
+ *    ↓
+ * Run test
+ *    ↓
+ * ROLLBACK
+ *
+ * instead of,
+ *
+ * Import SQL dump
+ *       ↓
+ *    Run test
+ *       ↓
+ * Import SQL dump again
+ *
+ */
 test.describe.configure({ mode: 'serial' });
 
 test.describe('Database session', ()=>{
@@ -12,9 +36,7 @@ test.describe('Database session', ()=>{
             email: string;
         }>(`SELECT id, first_name, email FROM users WHERE id=$1`, [1]);
 
-        //console.log(result);
-
-        console.log(result.rows);
+        console.table(result.rows);
 
         expect(result.rowCount).toBe(1);
         expect(result.rows[0].first_name).toBe('John');
